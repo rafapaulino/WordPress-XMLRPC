dfeffaea3aa32dae90fdb6083de573ddc1efcf2a
Rafael Brigagao Paulino <rafael.paulino@FIESP.COM.BR>
2018-05-11 16:00:54 -0300

    Iniciando a construção da classe com o carregamento das bibliotecas necessárias

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+/*
+http://docs.guzzlephp.org/en/stable/overview.html#installation
+https://github.com/php-curl-class/php-curl-class
+https://github.com/Seldaek/monolog
+
+
+xmlrpc:
+https://code.tutsplus.com/articles/xml-rpc-in-wordpress--wp-25467
+https://linuxprograms.wordpress.com/2010/07/16/wordpress-xml-rpc/
+https://linuxprograms.wordpress.com/2010/08/11/wordpress-xmlrpc-metaweblog-newpost/
+https://www.tutorialspoint.com/xml-rpc/index.htm
+
+a requisição sempre terá um xml-enconde-request
+
+passos que precisam ser realizados
+
+se conectar ao blog - tem que retornar um sucesso ou não para a conexão
+em caso de falha informar no log os motivos
+
+enviar uma imagem para a biblioteca e informar o id da imagem para ser utilizado depois
+
+enviar um post para o site e vincular a imagem com o post + a categoria e tags informadas
+
+pegar a url do post
+
+deletar um post através do id -- caso eu queira, porém menos importante
+
+
+segunda etapa
+criar os posts agendados - não precisa ter o retorno da url
+
+utlizar os exemplos da pdo de conexao e update para escrever as chamadas da classe
+
+
+https://www.doctrine-project.org/projects/doctrine-orm/en/2.6/tutorials/getting-started.html
+
+*/
